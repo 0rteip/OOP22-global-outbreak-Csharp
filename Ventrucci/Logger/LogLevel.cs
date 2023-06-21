26347a6791719d1d8238e2d0c9d7661fd2f1ecfe
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-06-21 13:56:02 +0200

    Add ConsoleLogger

---
+++ Ventrucci/Logger/LogLevel.cs
@@ -0,0 +1,20 @@
+namespace OP22_global_outbreak_Csharp.Ventrucci.ConsoleLogger;
+
+/// <summary>
+/// Log Level.
+/// </summary>
+public enum LogLevel
+{
+    /// <summary>
+    /// Info.
+    /// </summary>
+    Info,
+    /// <summary>
+    /// Warning.
+    /// </summary>
+    Warning,
+    /// <summary>
+    /// Error.
+    /// </summary>
+    Error
+}
