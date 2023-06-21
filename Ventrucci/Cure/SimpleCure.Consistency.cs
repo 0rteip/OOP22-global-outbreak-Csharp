3d4a4f5a62eb41619d7394718072a77d8be8923a
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-06-21 14:10:49 +0200

    Add SimpleCure Consistency

---
+++ Ventrucci/Cure/SimpleCure.Consistency.cs
@@ -0,0 +1,50 @@
+using OP22_global_outbreak_Csharp.Ventrucci.ConsoleLogger;
+
+namespace OOP22_global_outbreak_Csharp.Ventrucci.Cure
+{
+    public partial class SimpleCure : ICure
+    {
+        private bool ConsistCheck()
+        {
+            if (_priorities.Count == 0)
+            {
+                ConsoleLogger.Logger.Log(LogLevel.Warning, "Priority list can't be empty");
+                return false;
+            }
+            if (_contributions.Count == 0)
+            {
+                ConsoleLogger.Logger.Log(LogLevel.Warning, "Regions list can't be empty");
+                return false;
+            }
+            if (_priorities.Where(el => el.Priority == _currentPriority).Count() != 1)
+            {
+                ConsoleLogger.Logger.Log(LogLevel.Warning, "Invalid current prioriry: current priority '" + _currentPriority + "' is not found in the priorities '" + _priorities + "'");
+                return false;
+            }
+            if (_necessaryBudget < _researchBudget)
+            {
+                ConsoleLogger.Logger.Log(LogLevel.Warning, "Research budget '" + _researchBudget + "' must be lower than necessary budget '" + _necessaryBudget + "'");
+                return false;
+            }
+
+            return CheckIfPositive(_dailyBudget, "dailyBudget")
+                    && CheckIfPositive(_numberOfMajorContributors, "numberOfMajorContributors")
+                    && CheckIfPositive(_researchersEfficiency, "researchersEfficiency")
+                    && CheckIfPositive(_necessaryBudget, "necessaryBudget")
+                    && CheckIfPositive(_researchBudget, "researchBudget")
+                    && CheckIfPositive(_currentPriority, "currentPriority")
+                    && CheckIfPositive(_daysBeforeStartResearch, "daysBeforeStartResearch");
+        }
+
+        private static bool CheckIfPositive(float number, string name)
+        {
+            if (number < 0)
+            {
+                ConsoleLogger.Logger.Log(LogLevel.Warning, "Value " + name + " can't be negative");
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
