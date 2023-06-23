2463b44cc21a49fa851d19e6aef697cb771b43e4
Luca Carabini <luca.carabini@studio.unibo.it>
2023-06-23 18:36:35 +0200

    Change Testing lib

--- carabini/carabini/Test/Test.cs
+++ carabini/carabini/Test/Test.cs
@@ -1,19 +1,13 @@
-using carabini.events;
+﻿using carabini.events;
 using carabini.region;
 using carabini.voyages;
-using region;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks.Dataflow;
-using voyageController;
+using NUnit.Framework;
 
-namespace Test
+namespace carabini.Test
 {
-    [TestClass]
+    [TestFixture]
     public class Test
     {
-      
-        private readonly Dictionary<string, Tuple<int, int>> sizeAndNameOfMeans = new();
         private readonly static string REGION1_NAME = "Canada";
         private readonly static int REGION1_COLOR = -62442;
         private readonly static int REGION1_PORTS = 1;
@@ -23,7 +17,7 @@ namespace Test
             "Stati Uniti"
         };
         private readonly static float REGION1_HUMID = 0.6f;
-        private readonly static float REGION1_HOT =  0.3f;
+        private readonly static float REGION1_HOT = 0.3f;
         private readonly static int REGION1_TOTAL_POPULATION = 35158304;
         private readonly static int REGION1_FACILITIES = 10;
         private readonly static float REGION1_POOR = 0.10f;
@@ -44,18 +38,14 @@ namespace Test
         private readonly static float REGION2_POOR = 0.10f;
         private readonly static float REGION2_URBAN = 0.8f;
         private readonly static float REGION2_CLOSE_MEANS = 0.8f;
-        private readonly static List<string> MEANS = new()
+        private static readonly List<string> MEANS = new()
         {
             "terra", "aereoporti", "porti"
         };
         private readonly static int MEAN_SIZE = 300;
         private readonly static int MEAN_TIMES = 4;
 
-        public Test()
-        {
-        }
-
-        private static Dictionary<string, Tuple<int, int>>  CreateDictionaryVoyages()
+        private static Dictionary<string, Tuple<int, int>> CreateDictionaryVoyages()
         {
             Dictionary<string, Tuple<int, int>> sizeAndNameOfMeans = new();
             MEANS.ForEach(k => sizeAndNameOfMeans.Add(k, new(MEAN_TIMES, MEAN_SIZE)));
@@ -64,8 +54,8 @@ namespace Test
         private static List<IRegion> CreateRegions()
         {
             List<IRegion> regions = new();
-            regions.Add(new RegionImpl(REGION1_TOTAL_POPULATION, REGION1_NAME, CreateDictionary(true, REGION1_AIRPORTS, REGION1_PORTS), REGION1_URBAN, REGION1_POOR, REGION1_COLOR, 
-                        REGION1_FACILITIES ,REGION1_HOT, REGION1_HUMID, REGION1_CLOSE_MEANS ));
+            regions.Add(new RegionImpl(REGION1_TOTAL_POPULATION, REGION1_NAME, CreateDictionary(true, REGION1_AIRPORTS, REGION1_PORTS), REGION1_URBAN, REGION1_POOR, REGION1_COLOR,
+                        REGION1_FACILITIES, REGION1_HOT, REGION1_HUMID, REGION1_CLOSE_MEANS));
             regions.Add(new RegionImpl(REGION2_TOTAL_POPULATION, REGION2_NAME, CreateDictionary(false, REGION2_AIRPORTS, REGION2_PORTS), REGION2_URBAN, REGION2_POOR, REGION2_COLOR,
                         REGION2_FACILITIES, REGION2_HOT, REGION2_HUMID, REGION2_CLOSE_MEANS));
             return regions;
@@ -80,9 +70,9 @@ namespace Test
                 borders = REGION2_BORDERS;
             }
             reachableRegion.Add(MEANS[0], new(1, borders));
-            if(air > 0)
+            if (air > 0)
             {
-            reachableRegion.Add(MEANS[1], new(air, new()));
+                reachableRegion.Add(MEANS[1], new(air, new()));
             }
             if (port > 0)
             {
@@ -91,7 +81,7 @@ namespace Test
             return reachableRegion;
 
         }
-        [TestMethod]
+        [Test]
         public void RegionTest()
         {
             IRegion region = CreateRegions()[0];
@@ -101,7 +91,7 @@ namespace Test
 
         }
 
-        [TestMethod]
+        [Test]
         public void VoyagesTest()
         {
             IVoyages means = new VoyagesImpl(CreateDictionaryVoyages());
@@ -117,16 +107,16 @@ namespace Test
             {
                 Assert.IsTrue(means.GetMeans().Contains(elem.Key));
             }
-            regions.ForEach(k=> {
+            regions.ForEach(k => {
                 k.IncOrDecInfectedPeople(k.GetPopTot());
             });
             Console.WriteLine(regions[0].GetNumInfected());
-             List<IVoyage> vo = means.ExtractVoyages(regions, pot);
-             vo.ForEach(k => Console.WriteLine("means " + k.GetMeans() + " infected " + k.GetInfected()  +  " part " + k.GetPart() + " dest " +
-             k.GetDest()));
+            List<IVoyage> vo = means.ExtractVoyages(regions, pot);
+            vo.ForEach(k => Console.WriteLine("means " + k.GetMeans() + " infected " + k.GetInfected() + " part " + k.GetPart() + " dest " +
+            k.GetDest()));
         }
 
-        [TestMethod]
+        [Test]
         public void EventTest()
         {
             List<IEvent> events = new();
@@ -137,8 +127,10 @@ namespace Test
             events.Add(new EventImpl("uragano", 1, 0.0001f));
             ICauseEvent cause = new CauseEventImpl(events);
             IExtractedEvent? extractedEvent = cause.CauseEvent(regions);
-            Assert.IsNotNull(extractedEvent);
-            Assert.AreEqual((long) Math.Floor(regions[0].GetPopTot() * 0.0001f), extractedEvent.GetDeath());
+            if(extractedEvent != null)
+            {
+            Assert.AreEqual((long)Math.Floor(regions[0].GetPopTot() * 0.0001f), extractedEvent.GetDeath());
+            }
         }
     }
 }
